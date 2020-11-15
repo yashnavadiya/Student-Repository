392fad69d3a35e37ea3e1a0c72c2f87ca0414508
yash navadiya <60712784+yashnavadiya@users.noreply.github.com>
2020-11-15 15:26:19 -0500

    HW10 Test

--- Student_Repository_Test_Yash_Navadiya.py
+++ Student_Repository_Test_Yash_Navadiya.py
@@ -1,44 +1,75 @@
-"""
-Test File to test functions Date Arithmetic, File Reader, File Analyzer
-"""
+"""" Test for Repository """
 
 import unittest
-from HW08_Yash_Navadiya import date_arithmetic, file_reader, FileAnalyzer
 import os
+from HW10_Yash_Navadiya import Repository, Student, Instructor, Major
 
 
-class testFuctions(unittest.TestCase):
-    """To Implement the different test cases
-    """
+class TestRepository(unittest.TestCase):
+    """ Test for repository """
 
-    def test_date_arithmetic(self) -> None:
-        """Test the date arthimetic"""
-        self.assertEqual(date_arithmetic(), ('The date three days after Feb 27, 2020 is Mar 01, 2020',
-                                             'The date three days after Feb 27, 2019 is Mar 02, 2019', '241 days passed between Feb 01, 2019 and Sep 30, 2019'))
+    def setUp(self) -> None:
+        """This methods allow you to define instructions that will be executed before and after each test method"""
+        self.test_path: str = "HW10_Test"
+        self.repo: Repository = Repository(self.test_path, False)
 
-    def test_file_reader(self) -> None:
-        """ Test the file reader"""
-        expected = [('123', 'Jin He', 'Computer Science'), ('234', 'Nanda Koka',
-                                                            'Software Engineering'), ('345', 'Benji Cai', 'Software Engineering')]
-        self.assertEqual(list(file_reader(
-            'D:\yash\Stevens\SEM 3\SSW 810\Week 8\HW08_Test.txt', 3, '|', True)), expected)
-        self.assertNotEqual(list(file_reader(
-            'D:\yash\Stevens\SEM 3\SSW 810\Week 8\HW08_Test.txt', 3, '|')), expected)
-        with self.assertRaises(ValueError):
-            list(file_reader(
-                'D:\yash\Stevens\SEM 3\SSW 810\Week 8\HW08_Test.txt', 4, '|', True))
-        with self.assertRaises(FileNotFoundError):
-            list(file_reader('HW08_Test1.txt', 3, '|', True))
+    def test_majors(self) -> None: 6
+    """ Testing majors table"""
+    expected = [['SFEN', ['SSW 540', 'SSW 555', 'SSW 564', 'SSW 567'],
+                 ['CS 501', 'CS 513', 'CS 545']],
+                ['SYEN', ['SYS 612', 'SYS 671', 'SYS 800'],
+                 ['SSW 540', 'SSW 565', 'SSW 810']]]
 
-    def test_file_analyzer(self) -> None:
-        """ Test the file analyzer """
-        test = FileAnalyzer('HW08_Test')
-        expected = {'0_p.py': {'classes': 0, 'functions': 0, 'lines': 4, 'characters': 59},
-                    'file1.py': {'classes': 2, 'functions': 4, 'lines': 25, 'characters': 269}}
-        self.assertEqual(test.files_summary, expected)
-        with self.assertRaises(FileNotFoundError):
-            FileAnalyzer(' ')
+    calculated = [majors.info()
+                  for majors in self.repo._majors.values()]
+    self.assertEqual(expected, calculated)
+
+    def test_student_attributes(self) -> None:
+        """ Testing for student attributes """
+
+        expected = [['10103', 'Baldwin, C', 'SFEN', ['CS 501', 'SSW 564', 'SSW 567', 'SSW 687'], ['SSW 540', 'SSW 555'], [], 3.44],
+                    ['10115', 'Wyatt, X', 'SFEN', ['CS 545', 'SSW 564',
+                                                   'SSW 567', 'SSW 687'], ['SSW 540', 'SSW 555'], [], 3.81],
+                    ['10172', 'Forbes, I', 'SFEN', ['SSW 555', 'SSW 567'], [
+                        'SSW 540', 'SSW 564'], ['CS 501', 'CS 513', 'CS 545'], 3.88],
+                    ['10175', 'Erickson, D', 'SFEN', ['SSW 564', 'SSW 567', 'SSW 687'], [
+                        'SSW 540', 'SSW 555'], ['CS 501', 'CS 513', 'CS 545'], 3.58],
+                    ['10183', 'Chapman, O', 'SFEN', ['SSW 689'], ['SSW 540', 'SSW 555',
+                                                                  'SSW 564', 'SSW 567'], ['CS 501', 'CS 513', 'CS 545'], 4.0],
+                    ['11399', 'Cordova, I', 'SYEN', ['SSW 540'], [
+                        'SYS 612', 'SYS 671', 'SYS 800'], [], 3.0],
+                    ['11461', 'Wright, U', 'SYEN', ['SYS 611', 'SYS 750', 'SYS 800'], [
+                        'SYS 612', 'SYS 671'], ['SSW 540', 'SSW 565', 'SSW 810'], 3.92],
+                    ['11658', 'Kelly, P', 'SYEN', [], ['SYS 612', 'SYS 671',
+                                                       'SYS 800'], ['SSW 540', 'SSW 565', 'SSW 810'], 0.0],
+                    ['11714', 'Morton, A', 'SYEN', ['SYS 611', 'SYS 645'], ['SYS 612',
+                                                                            'SYS 671', 'SYS 800'], ['SSW 540', 'SSW 565', 'SSW 810'], 3.0],
+                    ['11788', 'Fuller, E', 'SYEN', ['SSW 540'], ['SYS 612', 'SYS 671', 'SYS 800'], [], 4.0]]
+
+        calculated = [student.info()
+                      for cwid, student in self.repo._students.items()]
+        self.assertEqual(expected, calculated)
+
+    def test_instructor_attributes(self) -> None:
+        """ Testing for Instructor attributes """
+        expected = [('98765', 'Einstein, A', 'SFEN', 'SSW 567', 4),
+                    ('98765', 'Einstein, A', 'SFEN', 'SSW 540', 3),
+                    ('98764', 'Feynman, R', 'SFEN', 'SSW 564', 3),
+                    ('98764', 'Feynman, R', 'SFEN', 'SSW 687', 3),
+                    ('98764', 'Feynman, R', 'SFEN', 'CS 501', 1),
+                    ('98764', 'Feynman, R', 'SFEN', 'CS 545', 1),
+                    ('98763', 'Newton, I',  'SFEN', 'SSW 555', 1),
+                    ('98763', 'Newton, I', 'SFEN', 'SSW 689', 1),
+                    ('98760', 'Darwin, C', 'SYEN', 'SYS 800', 1),
+                    ('98760', 'Darwin, C', 'SYEN', 'SYS 750', 1),
+                    ('98760', 'Darwin, C', 'SYEN', 'SYS 611', 2),
+                    ('98760', 'Darwin, C', 'SYEN', 'SYS 645', 1)]
+
+        calculated = [tuple(detail) for instructor in self.repo._instructors.values(
+        ) for detail in instructor.info()]
+        self.assertEqual(expected, calculated)
 
 
 if __name__ == "__main__":
+    """ Run test cases on startup """
     unittest.main(exit=False, verbosity=2)
